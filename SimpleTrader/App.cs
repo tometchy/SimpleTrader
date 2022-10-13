4b01fb2da983ee3776712e2d37d1651f5ce6b681
Tometchy <tometchy@gmail.com>
2022-10-13 16:40:27 +0200

    Use one http client per application

--- SimpleTrader/App.cs
+++ SimpleTrader/App.cs
@@ -3,25 +3,16 @@ using System.Linq;
 using Akka.Actor;
 using Akka.Event;
 using Kraken.Net.Clients;
-using Kraken.Net.Objects;
-using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
 namespace SimpleTrader
 {
     public class App : ReceiveActor
     {
-        public App()
+        public App(KrakenClient krakenClient)
         {
             Context.GetLogger().Info("Creating app");
             // Context.ActorOf(Props.Create(() => new UsersRepository()), nameof(UsersRepository));
-            var krakenClient = new KrakenClient(new KrakenClientOptions()
-            {
-                // ApiCredentials = new ApiCredentials("API-KEY", "API-SECRET"),
-                LogLevel = LogLevel.Trace,
-                RequestTimeout = TimeSpan.FromSeconds(60)
-            });
             
-            // Getting ticker
             var tickerData = krakenClient.SpotApi.ExchangeData.GetTickerAsync("XBTUSD").Result;
             Console.WriteLine(tickerData.Data.First().Value.LastTrade.Price);
         }
--- SimpleTrader/Program.cs
+++ SimpleTrader/Program.cs
@@ -1,6 +1,16 @@
 using Akka.Actor;
 using Infrastructure;
 using SimpleTrader;
+using CryptoExchange.Net.Authentication;
+using Kraken.Net.Clients;
+using Kraken.Net.Objects;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
-var appProcess = new SyncAppProcess(new AppBridge(Props.Create(() => new App())));
-appProcess.StartAndWaitForTermination();
+new SyncAppProcess(new AppBridge(Props.Create(() => new App(new KrakenClient(new KrakenClientOptions
+{
+    ApiCredentials = new ApiCredentials(
+        Environment.GetEnvironmentVariable("KRAKEN_API_KEY")!,
+        Environment.GetEnvironmentVariable("KRAKEN_API_KEY_SECRET")!),
+    LogLevel = LogLevel.Trace,
+    RequestTimeout = TimeSpan.FromSeconds(20)
+}))))).StartAndWaitForTermination();
