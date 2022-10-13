ddd4d5accb9bd23abeb1131119d59a86d9b604ef
Tometchy <tometchy@gmail.com>
2022-10-13 21:48:04 +0200

    Simplify running

--- build.sh
+++
@@ -1,5 +0,0 @@
-if [ -f "Docker/.env" ]; then
-	sudo docker-compose -f Docker/docker-compose.yml build
-else 
-    echo "You must create Docker/.env file with proper variables first"
-fi
--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-sudo docker-compose -f Docker/docker-compose.yml up
+if [ -f "Docker/.env" ]; then
+	sudo docker-compose -f Docker/docker-compose.yml up --build -d
+else 
+    echo "You must create Docker/.env file with proper variables first"
+fi
