5a97379953c895a08748cb3f4009191dc1b99982
Tometchy <tometchy@gmail.com>
2022-10-14 15:03:20 +0200

    Adjust to new format setting

--- SimpleTraderTests/BetParametersCreationTests.cs
+++ SimpleTraderTests/BetParametersCreationTests.cs
@@ -2,6 +2,7 @@ using System.Globalization;
 using NUnit.Framework;
 using SimpleTrader;
 using static SimpleTrader.BetParameters.BetType;
+
 // ReSharper disable ObjectCreationAsStatement
 // ReSharper disable ClassNeverInstantiated.Global
 
@@ -20,8 +21,7 @@ public class BetParametersCreationTests
             double threshold = 5;
             double amount = 10000;
 
-            var bet = new BetParameters(
-                cryptoTicker,
+            var bet = new BetParameters(cryptoTicker,
                 betType.ToString(),
                 initialPriceUsd.ToString(CultureInfo.InvariantCulture),
                 threshold.ToString(CultureInfo.InvariantCulture),
@@ -36,14 +36,11 @@ public class BetParametersCreationTests
 
         [Test]
         public void Converting_parameters_should_fail_due_to_malformed_bet_type() =>
-            Assert.Throws<ArgumentException>(
-                () => new BetParameters(
-                    F.Create<string>(),
-                    "MALFORMED",
-                    F.Create<decimal>().ToString(CultureInfo.InvariantCulture),
-                    F.Create<double>().ToString(CultureInfo.InvariantCulture),
-                    F.Create<double>().ToString(CultureInfo.InvariantCulture)
-                ));
+            Assert.Throws<ArgumentException>(() => new BetParameters(F.Create<string>(),
+                "MALFORMED",
+                F.Create<decimal>().ToString(CultureInfo.InvariantCulture),
+                F.Create<double>().ToString(CultureInfo.InvariantCulture),
+                F.Create<double>().ToString(CultureInfo.InvariantCulture)));
 
         [TestCase(Short, 100.0)]
         [TestCase(Long, 0.5)]
@@ -51,14 +48,11 @@ public class BetParametersCreationTests
             BetParameters.BetType confusedBetType,
             decimal amount)
         {
-            Assert.Throws<ArgumentException>(
-                () => new BetParameters(
-                    F.Create<string>(),
-                    confusedBetType.ToString(),
-                    F.Create<decimal>().ToString(CultureInfo.InvariantCulture),
-                    F.Create<double>().ToString(CultureInfo.InvariantCulture),
-                    amount.ToString(CultureInfo.InvariantCulture)
-                ));
+            Assert.Throws<ArgumentException>(() => new BetParameters(F.Create<string>(),
+                confusedBetType.ToString(),
+                F.Create<decimal>().ToString(CultureInfo.InvariantCulture),
+                F.Create<double>().ToString(CultureInfo.InvariantCulture),
+                amount.ToString(CultureInfo.InvariantCulture)));
         }
     }
 }
