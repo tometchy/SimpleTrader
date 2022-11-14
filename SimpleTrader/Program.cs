9e2c29591e4a8daed0e4559ac4bf3b92004d60fb
Tometchy <tometchy@gmail.com>
2022-11-14 16:41:41 +0100

    Allow running without api key

--- SimpleTrader/Program.cs
+++ SimpleTrader/Program.cs
@@ -4,16 +4,22 @@ using SimpleTrader;
 using CryptoExchange.Net.Authentication;
 using Kraken.Net.Clients;
 using Kraken.Net.Objects;
+using static System.String;
 using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
 var krakenSocketClient = new KrakenSocketClient();
-var krakenAdapter = new KrakenAdapter(new KrakenClient(new KrakenClientOptions
-{
-    ApiCredentials = new ApiCredentials(Environment.GetEnvironmentVariable("KRAKEN_API_KEY")!,
-        Environment.GetEnvironmentVariable("KRAKEN_API_KEY_SECRET")!),
-    LogLevel = LogLevel.Trace,
-    RequestTimeout = TimeSpan.FromSeconds(20)
-}));
+
+string krakenApiKey = Environment.GetEnvironmentVariable("KRAKEN_API_KEY") ?? Empty;
+string krakenApiKeySecret = Environment.GetEnvironmentVariable("KRAKEN_API_KEY_SECRET") ?? Empty;
+var krakenClientOptions = IsNullOrWhiteSpace(krakenApiKey) || IsNullOrWhiteSpace(krakenApiKeySecret)
+    ? new KrakenClientOptions { LogLevel = LogLevel.Trace, RequestTimeout = TimeSpan.FromSeconds(20) }
+    : new KrakenClientOptions
+    {
+        ApiCredentials = new ApiCredentials(krakenApiKey, krakenApiKeySecret),
+        LogLevel = LogLevel.Trace,
+        RequestTimeout = TimeSpan.FromSeconds(20)
+    };
+var krakenAdapter = new KrakenAdapter(new KrakenClient(krakenClientOptions));
 
 new SyncAppProcess(new AppBridge(Props.Create(() =>
         new App(krakenSocketClient, krakenAdapter, new ExchangeBridge(krakenAdapter)))))
