197cefbffd2a9f94af2be723b723e1e2a42ab040
Tometchy <tometchy@gmail.com>
2022-12-06 21:41:56 +0100

    Reduce number of detectors

--- SimpleTrader/Bet/BetsSupervisor.cs
+++ SimpleTrader/Bet/BetsSupervisor.cs
@@ -81,14 +81,14 @@ public class BetsSupervisor : ReceiveActor
         // CreateFixedPercentageFixedLookBackTrendDetector(FromMinutes(2), 4.75m);
         // CreateFixedPercentageFixedLookBackTrendDetector(FromMinutes(2), 7.77m);
 
-        CreateExtraFixedPercentageFixedLookBackTrendDetector(FromMinutes(1), 3.75m, FromMinutes(20));
+        // CreateExtraFixedPercentageFixedLookBackTrendDetector(FromMinutes(1), 3.75m, FromMinutes(20));
         CreateExtraFixedPercentageFixedLookBackTrendDetector(FromMinutes(1), 2.75m, FromMinutes(60));
         // CreateExtraFixedPercentageFixedLookBackTrendDetector(FromMinutes(1), 2.75m, FromMinutes(90));
         // CreateExtraFixedPercentageFixedLookBackTrendDetector(FromMinutes(1), 2.75m, FromHours(2));
         // CreateExtraFixedPercentageFixedLookBackTrendDetector(FromMinutes(1), 2.75m, FromHours(4));
 
-        CreateExtraFixedPercentageFixedLookBackTrendDetector(FromMinutes(4), 4.75m, FromHours(4));
-        CreateExtraFixedPercentageFixedLookBackTrendDetector(FromMinutes(4), 5.75m, FromHours(4));
+        // CreateExtraFixedPercentageFixedLookBackTrendDetector(FromMinutes(4), 4.75m, FromHours(4));
+        // CreateExtraFixedPercentageFixedLookBackTrendDetector(FromMinutes(4), 5.75m, FromHours(4));
         CreateExtraFixedPercentageFixedLookBackTrendDetector(FromMinutes(7), 7.77m, FromHours(7));
 
         // void CreateFixedPercentageFixedLookBackTrendDetector(TimeSpan howLongToLookBack, decimal percentageToCross) =>
